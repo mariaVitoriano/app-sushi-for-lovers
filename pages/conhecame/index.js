8ebafd0b5f3863f971049b6bf6037005b6ceb533
Mariaaa <164949216+mariaVitoriano@users.noreply.github.com>
2024-10-04 19:36:26 -0300

    Add files via upload

--- pages/conhecame/index.js
+++ pages/conhecame/index.js
@@ -1,199 +1,274 @@
 import { StatusBar } from 'expo-status-bar';
 import { useLoadFonts } from '../../componentes/fonts';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { useState } from 'react';
 
 const card = require('../../assets/img/card-1.png');
 const card2 = require('../../assets/img/card-2.png');
 const card3 = require('../../assets/img/card-3.png');
 const casal = require('../../assets/img/casalfeliz.jpg');
 
-export default function Cardapio(){
+export default function Cardapio() {
   const fontsLoaded = useLoadFonts();
+  const [selectedBase, setSelectedBase] = useState(null);
+  const [selectedProtein, setSelectedProtein] = useState(null);
+  const [selectedTopping, setSelectedTopping] = useState(null);
+
   return (
     <ScrollView>
-    <StatusBar style='light'/>
-    <View style={styles.container}>
-    <Text style={styles.tittle}>Conheca me</Text>
-    <View style={styles.list}>
-    <View style={styles.imageContainer}>
-           <TouchableOpacity>
-             <Image source={card3} style={styles.icon} />
-           </TouchableOpacity>
-
-           <View style={styles.textContainer}>
-             <Text style={styles.text1}>恋人のための寿司</Text>
-             <Text style={styles.text}>Sushi para amantes</Text>
-           </View>
-         </View>
-
-         <View style={styles.imageContainer}>
-           <TouchableOpacity>
-             <Image source={card2} style={styles.icon} />
-           </TouchableOpacity>
-
-      
-           <View style={styles.textContainerr}>
-             <Text style={styles.text2}>ソリティアの寿司</Text>
-             <Text style={styles.text}>Sushi para amigos</Text>
-           </View>
-         </View>
-
-         <View style={styles.imageContainer}>
-           <TouchableOpacity>
-            <Image source={card} style={styles.icon2} />
-           </TouchableOpacity>
-
-           <View style={styles.textContainer}>
-             <Text style={styles.text1}>ソリティアの寿司</Text>
-             <Text style={styles.text}>Sushi para solitários</Text>
+      <StatusBar style='light'/>
+      <View style={styles.container}>
+        <Text style={styles.tittle}>Conheça me</Text>
+        <View style={styles.list}>
+          {/* Card 1 */}
+          <View style={styles.imageContainer}>
+            <TouchableOpacity>
+              <Image source={card3} style={styles.icon} />
+            </TouchableOpacity>
+            <View style={styles.textContainer}>
+              <Text style={styles.text1}>恋人のための寿司</Text>
+              <Text style={styles.text}>Sushi para amantes</Text>
+            </View>
           </View>
-          <Image source={casal} style={styles.icon3} />
 
+          {/* Card 2 */}
+          <View style={styles.imageContainer}>
+            <TouchableOpacity>
+              <Image source={card2} style={styles.icon} />
+            </TouchableOpacity>
+            <View style={styles.textContainerr}>
+              <Text style={styles.text2}>ソリティアの寿司</Text>
+              <Text style={styles.text01}>Sushi para amigos</Text>
+            </View>
+          </View>
 
-         </View>
+          {/* Card 3 */}
+          <View style={styles.imageContainer}>
+            <TouchableOpacity>
+              <Image source={card} style={styles.icon2} />
+            </TouchableOpacity>
+            <View style={styles.textContainer}>
+              <Text style={styles.text1}>ソリティアの寿司</Text>
+              <Text style={styles.text}>Sushi para solitários</Text>
+            </View>
+            
+          </View>
+        </View>
 
-    </View>
-    </View>
+        {/* Nova seção: Monte Seu Sushi */}
+        <View style={styles.customSushiContainer}>
+          <Text style={styles.customTitle}>Monte seu Sushi</Text>
+          <Text style={styles.subTitle}>Escolha seus ingredientes:</Text>
+
+          {/* Escolha de base */}
+          <View style={styles.optionContainer}>
+            <Text style={styles.optionTitle}>Base:</Text>
+            <TouchableOpacity
+              style={[styles.optionButton, selectedBase === 'Arroz' && styles.selectedOption]}
+              onPress={() => setSelectedBase('Arroz')}
+            >
+              <Text style={styles.optionText}>Arroz Tradicional</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.optionButton, selectedBase === 'Alga' && styles.selectedOption]}
+              onPress={() => setSelectedBase('Alga')}
+            >
+              <Text style={styles.optionText}>Alga</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Escolha de proteína */}
+          <View style={styles.optionContainer}>
+            <Text style={styles.optionTitle}>Proteína:</Text>
+            <TouchableOpacity
+              style={[styles.optionButton, selectedProtein === 'Salmão' && styles.selectedOption]}
+              onPress={() => setSelectedProtein('Salmão')}
+            >
+              <Text style={styles.optionText}>Salmão</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.optionButton, selectedProtein === 'Atum' && styles.selectedOption]}
+              onPress={() => setSelectedProtein('Atum')}
+            >
+              <Text style={styles.optionText}>Atum</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.optionButton, selectedProtein === 'Camarão' && styles.selectedOption]}
+              onPress={() => setSelectedProtein('Camarão')}
+            >
+              <Text style={styles.optionText}>Camarão</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Escolha de coberturas */}
+          <View style={styles.optionContainer}>
+            <Text style={styles.optionTitle}>Cobertura:</Text>
+            <TouchableOpacity
+              style={[styles.optionButton, selectedTopping === 'Cream Cheese' && styles.selectedOption]}
+              onPress={() => setSelectedTopping('Cream Cheese')}
+            >
+              <Text style={styles.optionText}>Cream Cheese</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.optionButton, selectedTopping === 'Gergelim' && styles.selectedOption]}
+              onPress={() => setSelectedTopping('Gergelim')}
+            >
+              <Text style={styles.optionText}>Gergelim</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Botão de confirmação */}
+          <TouchableOpacity style={styles.confirmButton}>
+            <Text style={styles.confirmButtonText}>Confirmar Personalização</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </ScrollView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:'white',
+    backgroundColor: 'white',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: '15%',
-    // paddingBottom: '10%'
   },
   tittle: {
-        fontSize: 30,  
-           color: '#c71127',
-            fontFamily: 'japona',
-            textAlign: 'center',
-            textShadowColor: '#000',   // Cor da sombra (preta neste caso)
-  textShadowOffset: { width: 2, height: 2 }, // Deslocamento da sombra
-  textShadowRadius: 4, 
+    fontSize: 45,
+    marginBottom: 20,
+    color: '#c71127',
+    fontFamily: 'japona',
+    textAlign: 'center',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 4,
+    lineHeight: 50, // Ajuste da altura da linha
+    paddingHorizontal: 10, // Adicionando padding horizontal
+    flexShrink: 1, // Permite que o texto diminua se necessário
   },
-
   icon: {
     height: 200,
     width: 380,
-    
     marginBottom: 10,
   },
-
   imageContainer: {
     position: 'relative',
-    top: 30
+    top: 30,
   },
-
   textContainer: {
     position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 100,
     top: 50,
     justifyContent: 'center',
     alignItems: 'center',
   },
-
   textContainerr: {
     position: 'absolute',
-    top: 0,
-    left: 110,
-    right: 0,
     top: 50,
     justifyContent: 'center',
     alignItems: 'center',
+    left: 110,
   },
-
   text: {
     color: 'white',
     fontSize: 17,
     fontWeight: 'bold',
     textAlign: 'center',
     padding: 10,
+    
+  },
+  text01: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginLeft:90,
+    padding: 10,
+    
   },
 
   text1: {
     color: 'white',
     fontSize: 12,
-    right: 35,
     fontWeight: 'bold',
     textAlign: 'center',
     padding: 10,
   },
-
   text2: {
     color: 'white',
     fontSize: 12,
-    left: 30,
     fontWeight: 'bold',
-    textAlign: 'center',
+    textAlign: 'left',
+    marginLeft:90,
     padding: 10,
   },
-  icon2:{
+  icon2: {
     height: 200,
     width: 380,
-    
     marginBottom: 10,
-
   },
-
-
   icon3: {
     height: 300,
     width: 380,
-    
     marginBottom: 100,
+  },
+  customSushiContainer: {
+    backgroundColor: '#fff',
+    padding: 30,
+    marginTop: 50,
+    alignItems: 'center',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    marginBottom: 100,
+    width: 360,
+  },
+  customTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#c71127',
+    marginBottom: 20,
+    fontFamily: 'japona',
+  },
+  subTitle: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  optionContainer: {
+    marginVertical: 10,
+  },
+  optionTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 5,
+  },
+  optionButton: {
+    backgroundColor: '#f5f5f5',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  selectedOption: {
+    backgroundColor: '#c71127',
+  },
+  optionText: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  confirmButton: {
+    backgroundColor: '#c71127',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  confirmButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
-// export default function Cardapio() {
-//   return (
-//     <View style={styles.container}>
-
-//       <Text style={{ fontFamily: 'japona', fontSize: 24,  }}>Conheça-me</Text>
-
-//       <View>
-
-//         <View style={styles.imageContainer}>
-//           <TouchableOpacity>
-//             <Image source={card3} style={styles.icon} />
-//           </TouchableOpacity>
-
-//           <View style={styles.textContainer}>
-//             <Text style={styles.text1}>恋人のための寿司</Text>
-//             <Text style={styles.text}>Sushi para amantes</Text>
-//           </View>
-//         </View>
-
-//         <View style={styles.imageContainer}>
-//           <TouchableOpacity>
-//             <Image source={card2} style={styles.icon} />
-//           </TouchableOpacity>
-
-//           <View style={styles.textContainerr}>
-//             <Text style={styles.text2}>ソリティアの寿司</Text>
-//             <Text style={styles.text}>Sushi para amigos</Text>
-//           </View>
-//         </View>
-
-//         <View style={styles.imageContainer}>
-//           <TouchableOpacity>
-//             <Image source={card} style={styles.icon} />
-//           </TouchableOpacity>
-
-//           <View style={styles.textContainer}>
-//             <Text style={styles.text1}>ソリティアの寿司</Text>
-//             <Text style={styles.text}>Sushi para solitários</Text>
-//           </View>
-//         </View>
-
-//       </View>
-
-//     </View>
-//   );
-// }
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,379 +1,181 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { useLoadFonts } from '../../componentes/fonts';
 import * as Animatable from 'react-native-animatable';
 
 const Foto = require('../../assets/img/inicialFoto.png');
-const juntos = require('../../assets/img/juntos.png');
 const nori = require('../../assets/img/nori.png');
 const camas = require('../../assets/img/camas.png');
 const pote = require('../../assets/img/pote.png');
 const pessoas = require('../../assets/img/pessoas.png');
 
- 
-export default function Home(){
+export default function Home() {
   const fontsLoaded = useLoadFonts();
-return(
-  <Animatable.View
+
+  return (
+    <Animatable.View
       animation={'fadeInUp'}
       delay={400}
       style={styles.container}
     >
-  <ScrollView>
-    <StatusBar style='light'/>
-      <View style={styles.container}>
-
-      <Text style={{ 
-            fontFamily: 'japona', 
-             fontSize: 30,  
-           color: '#c71127',
-            fontFamily: 'japona',
-            textAlign: 'center',
-            textShadowColor: '#000',   // Cor da sombra (preta neste caso)
-  textShadowOffset: { width: 2, height: 2 }, // Deslocamento da sombra
-  textShadowRadius: 4, 
-             }}>Sushi for lovers </Text>
+      <ScrollView>
+        <StatusBar style='light' />
+        <View style={styles.container}>
+          <Text style={styles.title}>Sushi for lovers</Text>
           <Image source={Foto} style={styles.img} />
-          <View style={styles.card5}>
-            </View>
 
           <View style={styles.container1}>
+            <Text style={styles.text}>
+              O sushi criado a milhares de anos, tem em sua influência o grande poder de unir pessoas.
+              O poder de encontrar amantes de sushi e uni-los, bem enrolados igual
+              um sushi de salmão e alga.
+            </Text>
 
-<Text style={styles.text}>
-            O sushi criado a milhares de anos, tem em sua influência o grande poder de unir pessoas.
-            O poder de encontrar amantes de sushi e uni-los, bem enrolados igual
-             um sushi de salmão e alga.
-          </Text>
+            {/* Card com imagem e texto */}
+            <View style={styles.card1}>
+              <TouchableOpacity>
+                <Image source={nori} style={styles.cardImage} />
+              </TouchableOpacity>
+              <Text style={styles.cardText}>Para os solitários</Text>
+            </View>
 
-          {/* Card com imagem e texto */}
-           <View style={styles.card1}>
-           <TouchableOpacity>
-            <Image source={nori} style={styles.cardImage} />
-            </TouchableOpacity>
-            <Text style={styles.cardText}>Para os solitários</Text>
-            
-           </View>
+            <View style={styles.card2}>
+              <TouchableOpacity>
+                <Image source={pote} style={styles.cardImage} />
+              </TouchableOpacity>
+              <Text style={styles.cardText}>Para dividir com alguém especial</Text>
+            </View>
 
-           <View style={styles.card2}>
-           <TouchableOpacity>
-            <Image source={pote} style={styles.cardImage} />
-            </TouchableOpacity>
-            <Text style={styles.cardText}>Para dividir com alguém especial</Text>
-           </View>
+            <View style={styles.card3}>
+              <TouchableOpacity>
+                <Image source={camas} style={styles.cardImage} />
+              </TouchableOpacity>
+              <Text style={styles.cardText}>Juntos e combinados</Text>
+            </View>
 
-          <View style={styles.card3}>
-          <TouchableOpacity>
-             <Image source={camas} style={styles.cardImage} />
-             </TouchableOpacity>
-           <Text style={styles.cardText}>Juntos e combinados</Text>
-           </View>
+            <Image source={pessoas} style={styles.roda} />
 
-
-           <Image source={pessoas} style={styles.roda} />
-           <View style={styles.quoteContainer}>
-   <Animatable.Text animation="fadeIn" delay={200} style={styles.quote}>
-     "Sushi é mais do que comida, é uma experiência."
-   </Animatable.Text>
-   <Animatable.Text animation="fadeIn" delay={400} style={styles.quote}>
-    "Compartilhar sushi é compartilhar amor."
-   </Animatable.Text>
-   
-
-   
- </View>
-      </View>
-    </View>
-  </ScrollView>
-  </Animatable.View>
-);
+            {/* Quotes */}
+            <View style={styles.quoteContainer}>
+              <Animatable.Text animation="fadeIn" delay={200} style={styles.quote}>
+                "Sushi é mais do que comida, é uma experiência."
+              </Animatable.Text>
+              <Animatable.Text animation="fadeIn" delay={400} style={styles.quote}>
+                "Compartilhar sushi é compartilhar amor."
+              </Animatable.Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </Animatable.View>
+  );
 }
-const styles = StyleSheet.create({ 
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',  // Definindo o fundo branco para o container principal
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: '12%',
-    paddingBottom: '10%'
+    paddingBottom: '10%',
+  },
+  title: {
+    fontFamily: 'japona',
+    fontSize: 50,
+    color: '#c71127',
+    textAlign: 'center',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 4,
+  },
+  img: {
+    height: 400,
+    width: 378,
+    resizeMode: 'contain',
+    textAlign: 'center',
   },
   text: {
-        fontSize: 18,
-        textAlign: "justify",
-        margin: 20,
-      },
-      img: {
-            height: 400,
-            width: 378,
-            resizeMode: 'contain',
-            textAlign: 'center',
-          },
-           // Estilo dos cards
-   card1: {
-     flexDirection: 'row',
+    fontSize: 18,
+    textAlign: "justify",
+    margin: 20,
+    color: '#333',
+  },
+  container1: {
+    backgroundColor: 'white', // Fundo branco para manter consistência
+    paddingBottom: 20,
+  },
+  card1: {
+    flexDirection: 'row',
     alignItems: 'center',
-     backgroundColor: '#010101',
-     padding: 10,
-     marginVertical: 10,
+    backgroundColor: '#010101',
+    padding: 10,
+    marginVertical: 10,
     borderRadius: 8,
-     shadowColor: '#000',
-     shadowOpacity: 0.2,
-     shadowRadius: 7,
-    elevation: 3,
-     marginHorizontal: 20,
-   },
-   card2: {
-     flexDirection: 'row',
-     alignItems: 'center',
-     backgroundColor: '#e6ccb3',
-     padding: 10,
-     marginVertical: 10,
-     borderRadius: 8,
-     shadowColor: '#000',
-     shadowOpacity: 0.2,
-     shadowRadius: 5,
-     elevation: 3,
-     marginHorizontal: 20,
-   },
-   card3: {
-     flexDirection: 'row',
-     alignItems: 'center',
-     backgroundColor: '#c71127',
-     padding: 10,
-     marginVertical: 10,
-     borderRadius: 8,     
-     shadowColor: '#000',
+    shadowColor: '#000',
     shadowOpacity: 0.2,
-     shadowRadius: 5,
-     elevation: 3,
-     marginHorizontal: 20,
-   },
-
-   cardImage: {
-     width: 80,
-     height: 80,
-     resizeMode: 'contain',
-     marginRight: 10,
-   },
-
-   cardText: {
-     fontSize: 18,
-     flex: 1,
-     color:'#ffffff'
-   },
-      roda:{
-     height: 500,
-     width: 380,
-     resizeMode: 'contain',
-     textAlign: 'center',
-    
-   },
-   quoteContainer: {
-        padding: 20,
-        borderRadius: 10,
-        marginHorizontal: 20,
-        alignItems: 'center',
-      },
-      quote: {
-        fontSize: 20,
-        fontStyle: 'italic',
-        color: '#333',
-        marginBottom: 10,
-        textAlign: 'center',
-        backgroundColor: '#e6ccb3',
-        borderRadius:20,
-      },
-      quot:{
-        fontSize: 18,
-        fontStyle: 'italic',
-        color: '#f6f6f6',
-        marginBottom: 10,
-        textAlign: 'center'
-      },
-      container1:{
-       backgroundColor:'white',
-       top:0.0
-      },
-    
+    shadowRadius: 7,
+    elevation: 3,
+    marginHorizontal: 20,
+  },
+  card2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#e6ccb3',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 3,
+    marginHorizontal: 20,
+  },
+  card3: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#c71127',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 3,
+    marginHorizontal: 20,
+  },
+  cardImage: {
+    width: 80,
+    height: 80,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+  cardText: {
+    fontSize: 18,
+    flex: 1,
+    color: '#ffffff',
+  },
+  roda: {
+    height: 500,
+    width: 380,
+    resizeMode: 'contain',
+    textAlign: 'center',
+  },
+  quoteContainer: {
+    padding: 20,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    backgroundColor: 'white', // Fundo branco para a área de citações
+  },
+  quote: {
+    fontSize: 20,
+    fontStyle: 'italic',
+    color: '#333',
+    marginBottom: 10,
+    textAlign: 'center',
+    backgroundColor: '#e6ccb3',
+    borderRadius: 20,
+    padding: 10,
+  },
 });
-
-// export default function Home() {
-//   const fontsLoaded = useLoadFonts();
-//   return (
-//     <Animatable.View
-//       animation={'fadeInUp'}
-//       delay={400}
-//       style={styles.container}
-//     >
-//       <SafeAreaView>
-//         <StatusBar/>
-//         <ScrollView>
-
-//           <Text style={{ 
-//             fontFamily: 'japona', 
-//             fontSize: 24,  
-//             color: '#c71127',
-//             fontFamily: 'japona',
-//             textAlign: 'center',
-//             }}>Sushi for lovers</Text>
-//           <Image source={Foto} style={styles.img} />
-
-//           <Text style={styles.text}>
-//             O sushi criado a milhares de anos, tem em sua influência o grande poder de unir pessoas.
-//             O poder de encontrar amantes de sushi e uni-los, bem enrolados igual
-//             um sushi de salmão e alga.
-//           </Text>
-
-//           {/* Card com imagem e texto */}
-//           <View style={styles.card1}>
-//             <Image source={nori} style={styles.cardImage} />
-//             <Text style={styles.cardText}>Delicioso Nori</Text>
-//           </View>
-
-//           <View style={styles.card2}>
-//             <Image source={pote} style={styles.cardImage} />
-//             <Text style={styles.cardText}>Pote Especial</Text>
-//           </View>
-
-//           <View style={styles.card3}>
-//             <Image source={camas} style={styles.cardImage} />
-//             <Text style={styles.cardText}>Camas Macias</Text>
-//           </View>
-
-
-//           <Image source={pessoas} style={styles.roda} />
-//           <View style={styles.quoteContainer}>
-//   <Animatable.Text animation="fadeIn" delay={200} style={styles.quote}>
-//     "Sushi é mais do que comida, é uma experiência."
-//   </Animatable.Text>
-//   <Animatable.Text animation="fadeIn" delay={400} style={styles.quote}>
-//     "Compartilhar sushi é compartilhar amor."
-//   </Animatable.Text>
-//   <Animatable.Text animation="fadeIn" delay={600} style={styles.quot}>
-//     "Envolvidos como um sushi, unidos para sempre."
-//   </Animatable.Text>
-// </View>
-
-
-//         </ScrollView>
-//       </SafeAreaView>
-//     </Animatable.View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-
-//   img: {
-//     height: 400,
-//     width: 378,
-//     resizeMode: 'contain',
-//     textAlign: 'center',
-//   },
-
-//   title: {
-//     top:30,
-//     fontSize: 24,
-//     color: '#c71127',
-//     fontWeight: 'bold',
-//     fontFamily: 'japona',
-//     textAlign: 'center',
-
-//   },
-
-//   text: {
-//     fontSize: 18,
-//     textAlign: "justify",
-//     margin: 20,
-//   },
-
-//   // Estilo dos cards
-//   card1: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     backgroundColor: '#010101',
-//     padding: 10,
-//     marginVertical: 10,
-//     borderRadius: 8,
-//     shadowColor: '#000',
-//     shadowOpacity: 0.2,
-//     shadowRadius: 7,
-//     elevation: 3,
-//     marginHorizontal: 20,
-//   },
-//   card2: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     backgroundColor: '#e6ccb3',
-//     padding: 10,
-//     marginVertical: 10,
-//     borderRadius: 8,
-//     shadowColor: '#000',
-//     shadowOpacity: 0.2,
-//     shadowRadius: 5,
-//     elevation: 3,
-//     marginHorizontal: 20,
-//   },
-//   card3: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     backgroundColor: '#c71127',
-//     padding: 10,
-//     marginVertical: 10,
-//     borderRadius: 8,
-//     shadowColor: '#000',
-//     shadowOpacity: 0.2,
-//     shadowRadius: 5,
-//     elevation: 3,
-//     marginHorizontal: 20,
-//   },
-
-//   cardImage: {
-//     width: 80,
-//     height: 80,
-//     resizeMode: 'contain',
-//     marginRight: 10,
-//   },
-
-//   cardText: {
-//     fontSize: 18,
-//     flex: 1,
-//     color:'#ffffff'
-//   },
-//   roda:{
-//     height: 500,
-//     width: 380,
-//     resizeMode: 'contain',
-//     textAlign: 'center',
-    
-//   },
-
-//   quoteContainer: {
-//     padding: 20,
-//     backgroundColor: '#f5f5f5',
-//     borderRadius: 10,
-   
-//     marginHorizontal: 20,
-//     alignItems: 'center',
-//   },
-//   quote: {
-//     fontSize: 17,
-//     fontStyle: 'italic',
-//     color: '#333',
-//     marginBottom: 10,
-//     textAlign: 'center',
-//     backgroundColor: '#e6ccb3',
-//     borderRadius:20,
-//   },
-//   quot:{
-//     fontSize: 18,
-//     fontStyle: 'italic',
-//     color: '#f6f6f6',
-//     marginBottom: 10,
-//     textAlign: 'center'
-//   },
-  
-// });
